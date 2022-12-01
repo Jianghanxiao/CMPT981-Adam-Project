71d1ff78d80b70930afad07b7ee0da43c465cf8f
sanjayharesh <sanjayharesh@gmail.com>
2022-12-01 09:17:29 -0800

    added code for full exp

--- adam_sgd_example.py
+++ adam_sgd_example.py
@@ -6,6 +6,7 @@ import torch
 import matplotlib.pyplot as plt
 import copy
 import math
+from collections import defaultdict
 
 import argparse
 from optmizer import Adam, SGD, MSGD, MSVAG, MSSD
@@ -49,174 +50,92 @@ def main(args):
         for g in optim.param_groups:
             g['lr'] = g['lr'] / np.sqrt(k)
 
-<<<<<<< Updated upstream
-    def get_optimizer(w, name, lr):
-        if name == 'sgd':
-            opt = SGD([w], lr=lr)
-        elif name == 'adam':
-            opt = Adam([w], lr=lr)
-        elif name == 'msgd':
-            opt = MSGD([w], lr=lr)
-        elif name == 'msvag':
-            opt = MSVAG([w], lr=lr)
-        elif name == 'mssd':
-            opt = MSSD([w], lr=lr)
-        else:
-            raise NotImplementedError
-        return opt
+    set_seed(args.seed)
+    run_seeds = np.random.randint(1000000, size=args.runs)
 
-    def eval_grad(w, name, lr, k):
-        
-        opt = get_optimizer(w, name, lr)
-        change_lr(opt, k)
-        
-        opt.zero_grad()
-        
-        loss =  fn(w)
-        loss.backward()
-        
-        gnorm = 0
-        grad_ls = []
-        for p in opt.param_groups[0]['params']:
-            if p.grad is not None:
-                param_norm = p.grad.data.norm(2)
-                gnorm += param_norm.item() ** 2
-                grad_ls.append(p.grad.data)
-        gnorm = gnorm ** (1. / 2)
-        
-        return gnorm, grad_ls
+    LOSSES = defaultdict(lambda : defaultdict(dict))
 
-    def eval_smooth(prev_w, w, name, lr, k, num_pts=1):
-        alphas = np.arange(1, num_pts+1)/(num_pts+1)
-        prev_gnorm, prev_grad_ls = eval_grad(prev_w, name, lr, k)
-        update_size = norm_diff([prev_w], [w])
-        max_smooth = -1
-        for alpha in alphas:
-            new_w = copy.deepcopy(prev_w)
-            with torch.no_grad():
-                new_w = alpha * prev_w + (1-alpha) * w
-            new_w = torch.tensor(new_w, requires_grad=True)
+    for name in optimizers:
+        for seed in run_seeds:
+            for lr in [1e-1]:#[1e-7, 1e-6, 1e-5, 9e-4, 3e-4, 1e-4, 9e-3, 3e-3, 1e-3, 9e-2, 3e-2, 1e-2, 0.9, 0.3, 0.1, 1.]:
                 
-            _, new_grad_ls = eval_grad(new_w, name, lr, k)
-            smooth = norm_diff(new_grad_ls, prev_grad_ls)/ (update_size * (1- alpha))
-            max_smooth = max(smooth, max_smooth)
-        
-        return max_smooth, prev_gnorm
-    
-    def norm_diff(list1, list2=None):
-        if not list2:
-            list2 = [0] * len(list1)
-        assert len(list1) == len(list2)
-        return math.sqrt(sum((list1[i]-list2[i]).norm()**2 for i in range(len(list1))))
+                print('\n\n------------------------')
+                print('LR: ', lr)
 
-    figure, axis = plt.subplots(1, 2, figsize=(12, 5))
+                set_seed(seed)
+                w = torch.randn((2, ), requires_grad=True)
 
-    for name in optimizers:
-        print(f'-----------------------------------')
-        print(name.upper())
-        set_seed(args.seed)
-        w = torch.randn((2, ), requires_grad=True)
-
-        opt = get_optimizer(w, name, args.lr)
-
-        losses = []
-        grad_norm_ls = []
-        smoothness_ls = []
-        for i in range(args.iters):
-            prev_w = copy.deepcopy(w)
-            # lr decay
-            change_lr(opt, k=i+1)
-
-            # zero grad
-            opt.zero_grad()
-
-            # function
-            loss =  fn(w)
-            print(f'Iter : {i} -- Loss : {loss}')
-
-            loss.backward()
-            opt.step()
-
-            losses.append(loss.item())
-            smoothness, fn_gradnorm = eval_smooth(prev_w, w, name, args.lr, i+1, num_pts=1)
-            grad_norm_ls.append(fn_gradnorm)
-            smoothness_ls.append(smoothness)
-
-        losses = np.log10(np.array(losses))
-        axis[0].plot(losses[np.logical_not(np.isnan(losses))], label=name)
-
-        grad_norm_ls = np.log10(np.array(grad_norm_ls))
-        smoothness_ls = np.log10(np.array(smoothness_ls))
-        
-        if len(grad_norm_ls[np.logical_not(np.isnan(grad_norm_ls))]) == len(smoothness_ls[np.logical_not(np.isnan(smoothness_ls))]):
-            axis[1].scatter(grad_norm_ls[np.logical_not(np.isnan(grad_norm_ls))],
-            smoothness_ls[np.logical_not(np.isnan(smoothness_ls))], label=name)
-    
-    axis[0].legend()
-    axis[0].set_xlabel('Iteration')
-    axis[0].set_ylabel('Log10 Loss')
-
-    axis[1].legend()
-    axis[1].set_xlabel('Log10 Grad Norm')
-    axis[1].set_ylabel('Log10 Smoothness')
-
-    plt.show()
-=======
-    for name in optimizers:
-        for lr in [1e-1]:#[1e-7, 1e-6, 1e-5, 9e-4, 3e-4, 1e-4, 9e-3, 3e-3, 1e-3, 9e-2, 3e-2, 1e-2, 0.9, 0.3, 0.1, 1.]:
-            
-            print('\n\n------------------------')
-            print('LR: ', lr)
-
-            set_seed(args.seed)
-            w = torch.randn((2, ), requires_grad=True)
-
-            if name == 'sgd':
-                opt = SGD([w], lr=lr, stochastic=args.stochastic, noise_std=args.noise_std)
-            elif name == 'adam':
-                opt = Adam([w], lr=lr, stochastic=args.stochastic, noise_std=args.noise_std)
-            elif name == 'msgd':
-                opt = MSGD([w], lr=lr, stochastic=args.stochastic, noise_std=args.noise_std)
-            elif name == 'msvag':
-                opt = MSVAG([w], lr=lr, stochastic=args.stochastic, noise_std=args.noise_std)
-            elif name == 'mssd':
-                opt = MSSD([w], lr=lr, stochastic=args.stochastic, noise_std=args.noise_std)
-            else:
-                raise NotImplementedError
-
-            losses = []
-            for i in range(5):
-
-                # lr decay
                 if name == 'sgd':
-                    change_lr(opt, k=i+1)
+                    opt = SGD([w], lr=lr, stochastic=args.stochastic, noise_std=args.noise_std)
+                elif name == 'adam':
+                    opt = Adam([w], lr=lr, stochastic=args.stochastic, noise_std=args.noise_std)
+                elif name == 'msgd':
+                    opt = MSGD([w], lr=lr, stochastic=args.stochastic, noise_std=args.noise_std)
+                elif name == 'msvag':
+                    opt = MSVAG([w], lr=lr, stochastic=args.stochastic, noise_std=args.noise_std)
+                elif name == 'mssd':
+                    opt = MSSD([w], lr=lr, stochastic=args.stochastic, noise_std=args.noise_std)
+                else:
+                    raise NotImplementedError
 
-                # zero grad
-                opt.zero_grad()
+                losses = []
+                for i in range(5):
 
-                # function
-                loss =  fn(w)
-                print(f'Iter : {i} -- Loss : {loss}')
+                    # lr decay
+                    if name == 'sgd':
+                        change_lr(opt, k=i+1)
 
-                loss.backward()
+                    # zero grad
+                    opt.zero_grad()
 
-                opt.step()
+                    # function
+                    loss =  fn(w)
+                    print(f'Iter : {i} -- Loss : {loss}')
 
-                losses.append(loss.item())
+                    loss.backward()
 
-            losses = np.log10(np.array(losses))
-            plt.plot(losses[np.logical_not(np.isnan(losses))], label=name+f"_{lr}")
-        
-        plt.legend()
-        plt.xlabel('Iteration')
-        plt.ylabel('Log10 Loss')
-        plt.show()
->>>>>>> Stashed changes
+                    opt.step()
+
+                    losses.append(loss.item())
+
+                LOSSES[name][seed][lr] = losses
+            
+
+    for name in LOSSES.keys():
+        for seed in LOSSES[name].keys():
+            min_loss = math.inf
+            min_lr = None
+            for lr, lr_losses in LOSSES[name][seed].items():
+                if np.mean(lr_losses) < min_loss:
+                    min_loss = np.mean(lr_losses)
+                    min_lr = lr
+            if min_lr is not None:
+                LOSSES[name][seed]['min_lr'] = LOSSES[name][seed][min_lr]
+            else:
+                LOSSES[name][seed]['min_lr'] = None
+
+        seed_losses = []
+        for seed in LOSSES[name].keys():
+            if LOSSES[name][seed]['min_lr'] is not None:
+                seed_losses.append(np.array(LOSSES[name][seed]['min_lr']))
+
+        LOSSES[name]['mean'] = np.mean(seed_losses, axis=0)
+    
+    
+    for name in LOSSES.keys():        
+        losses = np.log10(np.array(LOSSES[name]['mean']))
+        plt.plot(losses[np.logical_not(np.isnan(losses))], label=name)
+    
+    plt.legend()
+    plt.xlabel('Iteration')
+    plt.ylabel('Log10 Loss')
+    plt.show()
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--runs', type=int, default=1)
     parser.add_argument('--lr', type=float, default=0.01)
     parser.add_argument('--iters', type=int, default=5)
     parser.add_argument('--optimizer', type=str, default='all', choices=['sgd', 'adam', 'msgd', 'mssd', 'msvag', 'all'])
